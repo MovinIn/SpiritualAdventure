f034f910e4b6cfbfdeeeb673b5de9fae0f269aac
MovinIn <josephlam231@gmail.com>
2024-12-02 23:56:33 -0600

    added simple level 2 for demo

--- levels/Level2.cs
+++ levels/Level2.cs
@@ -1,16 +1,26 @@
 using Godot;
 using System;
+using System.Collections.Generic;
+using SpiritualAdventure.entities;
 using SpiritualAdventure.levels;
+using SpiritualAdventure.objects;
 
 public partial class Level2 : Level
 {
-	// Called when the node enters the scene tree for the first time.
-	public override void _Ready()
-	{
-	}
+  public override void _Ready()
+  {
+    GD.Print("loading objective");
+    var objective= TestTouchObjective(new Vector2(200,200));
+    LoadLevel(new Vector2(0,0),new List<IHasObjective>{objective},new List<Npc>(),new Narrator());
+    NextObjective();
+  }
 
-	// Called every frame. 'delta' is the elapsed time since the previous frame.
-	public override void _Process(double delta)
-	{
-	}
+  public IHasObjective TestTouchObjective(Vector2 position,int timeLimit=-1)
+  {
+    var touchObjective=TouchObjective.Instantiate(ObjectiveBuilder.TimedOrElse(
+      "Touch the Checkpoint at "+position,timeLimit));
+    touchObjective.Position = position;
+    AddChild(touchObjective);
+    return touchObjective;
+  }
 }
